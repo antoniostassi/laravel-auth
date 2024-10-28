7392f57744bd8a514422ecc1ead31c679814a5e5
Antonio Stassi <stassiant@outlook.it>
2024-10-28 14:26:31 +0100

    Model mass assignment fillable

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        // Mass Assignment
+        'name',
+        'description',
+        'expiring_date',
+        'label_tag',
+        'price',
+        'completed'
+    ];
 }
