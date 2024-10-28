a76cfd46c529e1381a7817ad4f01246ff88aabf8
Antonio Stassi <stassiant@outlook.it>
2024-10-28 14:26:38 +0100

    Migration project table

--- database/migrations/2024_10_28_130339_create_project_table.php
+++ database/migrations/2024_10_28_130339_create_project_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('project', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 64);
+            $table->string('description', 1024);
+            $table->date('creation_date');
+            $table->date('expiring_date')->nullable();
+            $table->string('label_tag')->nullable();
+            $table->integer('price')->unsigned()->default(0);
+            $table->bool('completed')->default(false);
             $table->timestamps();
         });
     }
