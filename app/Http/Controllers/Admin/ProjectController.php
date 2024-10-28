be1b49e91fac12fb44d69cb7453a67b0ca3340fd
Antonio Stassi <stassiant@outlook.it>
2024-10-28 15:04:44 +0100

    Project Controller adjustments

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -16,8 +16,8 @@ class ProjectController extends Controller
     public function index()
     {
         //
-        $project = Project::all();
-        return view('project.index', compact('project'));
+        $projects = Project::all();
+        return view('admin.projects.index', compact('projects'));
     }
 
     /**
